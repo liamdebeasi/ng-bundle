6c5518c01dab4377bfc2f1f33a5e8e870590f117
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-11-27 14:59:28 -0500

    add ionic repro

--- src/app/a/a.component.ts
+++ src/app/a/a.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonChip } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-a',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IonChip],
   templateUrl: './a.component.html',
   styleUrls: ['./a.component.css']
 })
--- src/app/b/b.component.ts
+++ src/app/b/b.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonBadge } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-b',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IonBadge],
   templateUrl: './b.component.html',
   styleUrls: ['./b.component.css']
 })
